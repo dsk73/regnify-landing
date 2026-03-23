7bf491ded6528c5888b96e862fd37697399f5314
dsk73 <dhananjayskokate@gmail.com>
2026-03-23 15:16:22 +0530

    Added proper Gap

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -186,7 +186,7 @@ export const Services = () => {
           </p>
         </motion.div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
           {services.map((service, index) => (
             <ServiceCard key={index} {...service} />
           ))}
